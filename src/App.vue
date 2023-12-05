211738baec84430d10c69a069eb291b06fa3c012
n0lk <89748313+nzerolk@users.noreply.github.com>
2023-12-04 19:09:24 -0500

    Landing page skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,23 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <Hero/>
+    <Features/>
+    <Team/>
+    <Footer/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script>
+import {defineComponent} from "vue";
+import Hero from "./components/Hero.vue";
+import Features from "./components/Features.vue";
+import Team from "./components/Team.vue";
+import Footer from "./components/Footer.vue";
+
+export default defineComponent({
+  components: {Footer, Team, Hero, Features}
+})
+</script>
+
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
