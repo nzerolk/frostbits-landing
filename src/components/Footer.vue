8fca36f7baa2c579b9216760827cfa46af405285
Wilder Stubbs <97481326+Wild-er@users.noreply.github.com>
2024-07-19 12:56:58 -0400

    Update Footer to include our X profile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,12 @@
     <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="mx-auto max-w-7xl px-6 pb-8 lg:px-8">
       <div class="border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
+        <div class="flex items-center space-x-6">
+          <a href="https://x.com/FrostbitsLtd" target="_blank" class="text-gray-400 hover:text-gray-300">
+            <span class="sr-only">X</span>
+            <img src="/assets/logo-white.png" alt="X Icon" class="h-6 w-6">
+          </a>
+        </div>
         <p class="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">&copy; 2023 Frostbits Solutions, Ltd. All rights reserved.</p>
       </div>
     </div>
